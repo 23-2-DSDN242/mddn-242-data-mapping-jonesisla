399f289fcc37684b0709854d3d274defb14f2753
jonesisla <139181549+jonesisla@users.noreply.github.com>
2023-09-21 11:15:21 +1200

    Testings Toms Code
    Adding in toms code into the sketch file. This code renders the images a lot clearer and with the progress bar.

--- sketch.js
+++ sketch.js
@@ -53,3 +53,64 @@ function keyTyped() {
     saveBlocksImages();
   }
 }
+
+
+
+// // tom's example in class
+// let sourceImg=null;
+// let maskImg=null;
+// let renderCounter=0;
+
+// // change these three lines as appropiate
+// let sourceFile = "input_1.jpg";
+// let maskFile   = "mask_1.png";
+// let outputFile = "output_1.png";
+
+// function preload() {
+//   sourceImg = loadImage(sourceFile);
+//   maskImg = loadImage(maskFile);
+// }
+
+// function setup () {
+//   let main_canvas = createCanvas(1920, 1080);
+//   main_canvas.parent('canvasContainer');
+
+//   imageMode(CENTER);
+//   noStroke();
+//   background(0, 0, 128);
+//   sourceImg.loadPixels();
+//   maskImg.loadPixels();
+// }
+
+// function draw () {
+//   let j = renderCounter;
+//   // get one scanline
+//   for(let i=0; i<1920; i++) {
+//     let pix = sourceImg.get(i, j);
+//     let mask = maskImg.get(i, j);
+//     if(mask[0] > 128) {
+//       // draw the full pixels
+//       set(i, j, pix);
+//     }
+//     else {
+//       // draw a "dimmed" version in gray
+//       let gray_color = 64 + pix[1] / 8;
+//       set(i, j, gray_color);
+//     }
+//   }
+//   updatePixels();
+//   renderCounter = renderCounter + 1;
+//   print(renderCounter);
+//   if(renderCounter > 1080) {
+//     console.log("Done!")
+//     noLoop();
+//     // uncomment this to save the result
+//     // saveArtworkImage(outputFile);
+//   }
+// }
+
+// function keyTyped() {
+//   if (key == '!') {
+//     saveBlocksImages();
+//   }
+// }
