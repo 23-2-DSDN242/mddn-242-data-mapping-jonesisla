a406369110db31854fe6afd11186815279254d4d
jonesisla <139181549+jonesisla@users.noreply.github.com>
2023-10-12 12:41:20 +1300

    Formatting and Backup
    Deleting all of my old commented code and making a backup to start testing out new layers

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
-//Warping Example
-
 let sourceImg=null;
 let maskImg=null;
+let renderCounter=0;
 
 // change these three lines as appropiate
 let sourceFile = "input_2.jpg";
@@ -25,6 +24,7 @@ function setup () {
   colorMode(HSB);
 }
 
+
 //render a chucnk
 // let X_STOP = 640;
 // let Y_STOP = 480;
@@ -34,15 +34,18 @@ let X_STOP = 1080;
 let Y_STOP = 1920;
 let OFFSET = 20;
 
-let renderCounter=0;
+
 function draw () {
   angleMode(DEGREES);
   let num_lines_to_draw = 40;
+  
   // get one scanline
   for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<Y_STOP; j++) {
     for(let i=0; i<X_STOP; i++) {
       colorMode(RGB);
       let mask = maskImg.get(i, j);
+      
+      
       if (mask[1] > 128) {
         pix = sourceImg.get(i, j);
       }
@@ -50,16 +53,14 @@ function draw () {
         let wave = sin(j*8);
         let slip = map(wave, -1, 1, -OFFSET, OFFSET);
         pix = sourceImg.get(i+slip, j);
-
-        // let brt = map(wave, -1, 1, 0, 255);
-        // for(let c=0; c<3; c++) {
-        //   pix[c] = brt;
-        // }
       }
+
 
       set(i, j, pix);
     }
   }
+
+
   renderCounter = renderCounter + num_lines_to_draw;
   updatePixels();
 
@@ -72,6 +73,7 @@ function draw () {
   }
 }
 
+
 function keyTyped() {
   if (key == '!') {
     saveBlocksImages();
@@ -80,17 +82,15 @@ function keyTyped() {
 
 
 
-
-
-//DEFAULT
+//Warping Backup
 // let sourceImg=null;
 // let maskImg=null;
 // let renderCounter=0;
 
 // // change these three lines as appropiate
-// let sourceFile = "input_1.jpg";
-// let maskFile   = "mask_1.png";
-// let outputFile = "output_1.png";
+// let sourceFile = "input_2.jpg";
+// let maskFile   = "mask_2.png";
+// let outputFile = "output_2.png";
 
 // function preload() {
 //   sourceImg = loadImage(sourceFile);
@@ -98,69 +98,7 @@ function keyTyped() {
 // }
 
 // function setup () {
-//   let main_canvas = createCanvas(1920, 1080);
-//   main_canvas.parent('canvasContainer');
-
-//   imageMode(CENTER);
-//   noStroke();
-//   background(255, 0, 0);
-//   sourceImg.loadPixels();
-//   maskImg.loadPixels();
-// }
-
-// function draw () {
-//   for(let i=0;i<4000;i++) {
-//     let x = floor(random(sourceImg.width));
-//     let y = floor(random(sourceImg.height));
-//     let pix = sourceImg.get(x, y);
-//     let mask = maskImg.get(x, y);
-//     fill(pix);
-//     if(mask[0] > 128) {
-//       let pointSize = 10;
-//       ellipse(x, y, pointSize, pointSize);
-//     }
-//     else {
-//       let pointSize = 20;
-//       rect(x, y, pointSize, pointSize);    
-//     }
-//   }
-//   renderCounter = renderCounter + 1;
-//   if(renderCounter > 10) {
-//     console.log("Done!")
-//     noLoop();
-//     // uncomment this to save the result
-//     // saveArtworkImage(outputFile);
-//   }
-// }
-
-// function keyTyped() {
-//   if (key == '!') {
-//     saveBlocksImages();
-//   }
-// }
-
-
-
-
-
-
-// // tom's example in class
-// let sourceImg=null;
-// let maskImg=null;
-// let renderCounter=0;
-
-// // change these three lines as appropiate
-// let sourceFile = "input_1.jpg";
-// let maskFile   = "mask_1.png";
-// let outputFile = "output_1.png";
-
-// function preload() {
-//   sourceImg = loadImage(sourceFile);
-//   maskImg = loadImage(maskFile);
-// }
-
-// function setup () {
-//   let main_canvas = createCanvas(1920, 1080);
+//   let main_canvas = createCanvas(1080, 1920);
 //   main_canvas.parent('canvasContainer');
 
 //   imageMode(CENTER);
@@ -168,34 +106,58 @@ function keyTyped() {
 //   background(0, 0, 128);
 //   sourceImg.loadPixels();
 //   maskImg.loadPixels();
+//   colorMode(HSB);
 // }
 
+
+// //render a chucnk
+// // let X_STOP = 640;
+// // let Y_STOP = 480;
+
+// //render full thing
+// let X_STOP = 1080;
+// let Y_STOP = 1920;
+// let OFFSET = 20;
+
+
 // function draw () {
-//   let j = renderCounter;
+//   angleMode(DEGREES);
+//   let num_lines_to_draw = 40;
+  
 //   // get one scanline
-//   for(let i=0; i<1920; i++) {
-//     let pix = sourceImg.get(i, j);
-//     let mask = maskImg.get(i, j);
-//     if(mask[0] > 128) {
-//       // draw the full pixels
+//   for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<Y_STOP; j++) {
+//     for(let i=0; i<X_STOP; i++) {
+//       colorMode(RGB);
+//       let mask = maskImg.get(i, j);
+      
+      
+//       if (mask[1] > 128) {
+//         pix = sourceImg.get(i, j);
+//       }
+//       else {
+//         let wave = sin(j*8);
+//         let slip = map(wave, -1, 1, -OFFSET, OFFSET);
+//         pix = sourceImg.get(i+slip, j);
+//       }
+
+      
 //       set(i, j, pix);
 //     }
-//     else {
-//       // draw a "dimmed" version in gray
-//       let gray_color = 64 + pix[1] / 8;
-//       set(i, j, gray_color);
-//     }
 //   }
+
+
+//   renderCounter = renderCounter + num_lines_to_draw;
 //   updatePixels();
-//   renderCounter = renderCounter + 1;
-//   print(renderCounter);
-//   if(renderCounter > 1080) {
+
+//   // print(renderCounter);
+//   if(renderCounter > Y_STOP) {
 //     console.log("Done!")
 //     noLoop();
 //     // uncomment this to save the result
 //     // saveArtworkImage(outputFile);
 //   }
 // }
+
 
 // function keyTyped() {
 //   if (key == '!') {
