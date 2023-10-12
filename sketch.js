184426ec6e8c1c2ae814c87c67860e9ff5b0b055
jonesisla <139181549+jonesisla@users.noreply.github.com>
2023-10-12 14:18:13 +1300

    updating to backup
    going back to what I originally had because even though its simple I like it.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
+//Warping Backup
 let sourceImg=null;
 let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "output_1.png";
+let sourceFile = "input_2.jpg";
+let maskFile   = "mask_2.png";
+let outputFile = "output_2.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -25,55 +26,45 @@ function setup () {
 }
 
 
-// //render a chucnk
-// // let X_STOP = 640;
-// // let Y_STOP = 480;
+//render a chucnk
+// let X_STOP = 640;
+// let Y_STOP = 480;
 
-// //render full thing
+//render full thing
 let X_STOP = 1080;
 let Y_STOP = 1920;
 let OFFSET = 20;
 
 
-
 function draw () {
+  angleMode(DEGREES);
   let num_lines_to_draw = 40;
+  
   // get one scanline
   for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<Y_STOP; j++) {
     for(let i=0; i<X_STOP; i++) {
       colorMode(RGB);
-      let pix = sourceImg.get(i, j);
-      // create a color from the values (always RGB)
-      let col = color(pix);
       let mask = maskImg.get(i, j);
-
-      colorMode(HSB, 360, 100, 100);
-      // draw a "dimmed" version in gray
-
-      if(mask[0] > 128) {
+      
+      
+      if (mask[1] > 128) {
         pix = sourceImg.get(i, j);
       }
       else {
-        let new_col = color(290,100,100);
-        //wave
         let wave = sin(j*8);
-        let slip = map(wave, -1,1, -OFFSET , OFFSET );
-        //pix = sourceImg.get(i+slip, j);
-        set(i+slip, j, new_col);
-    
+        let slip = map(wave, -1, 1, -OFFSET, OFFSET);
+        pix = sourceImg.get(i+slip, j);
       }
 
+      
       set(i, j, pix);
-
     }
   }
 
 
-
-
-
   renderCounter = renderCounter + num_lines_to_draw;
   updatePixels();
+
   // print(renderCounter);
   if(renderCounter > Y_STOP) {
     console.log("Done!")
@@ -82,6 +73,7 @@ function draw () {
     // saveArtworkImage(outputFile);
   }
 }
+
 
 function keyTyped() {
   if (key == '!') {
@@ -94,15 +86,17 @@ function keyTyped() {
 
 
 
-// //Warping Backup
+
+
+// //Warped Colour
 // let sourceImg=null;
 // let maskImg=null;
 // let renderCounter=0;
 
 // // change these three lines as appropiate
-// let sourceFile = "input_2.jpg";
-// let maskFile   = "mask_2.png";
-// let outputFile = "output_2.png";
+// let sourceFile = "input_1.jpg";
+// let maskFile   = "mask_1.png";
+// let outputFile = "output_1.png";
 
 // function preload() {
 //   sourceImg = loadImage(sourceFile);
@@ -122,45 +116,59 @@ function keyTyped() {
 // }
 
 
-// //render a chucnk
-// // let X_STOP = 640;
-// // let Y_STOP = 480;
+// // //render a chucnk
+// // // let X_STOP = 640;
+// // // let Y_STOP = 480;
 
-// //render full thing
+// // //render full thing
 // let X_STOP = 1080;
 // let Y_STOP = 1920;
-// let OFFSET = 20;
+// let OFFSET = 40;
+
 
 
 // function draw () {
-//   angleMode(DEGREES);
 //   let num_lines_to_draw = 40;
-  
 //   // get one scanline
 //   for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<Y_STOP; j++) {
 //     for(let i=0; i<X_STOP; i++) {
 //       colorMode(RGB);
+//       let pix = sourceImg.get(i, j);
+//       // create a color from the values (always RGB)
+//       let col = color(pix);
 //       let mask = maskImg.get(i, j);
-      
-      
-//       if (mask[1] > 128) {
+
+//       colorMode(HSB, 360, 100, 100);
+//       // draw a "dimmed" version in gray
+
+//       if(mask[0] > 128) {
 //         pix = sourceImg.get(i, j);
 //       }
-//       else {
-//         let wave = sin(j*8);
-//         let slip = map(wave, -1, 1, -OFFSET, OFFSET);
-//         pix = sourceImg.get(i+slip, j);
+//       else if (mask[1] < 128) {
+//         let new_col = color(290,100,100);
+//         //wave
+//         let wave = sin(j*4);
+//         let slip = map(wave, -2,2, -OFFSET , OFFSET );
+//         //pix = sourceImg.get(i+slip, j);
+//         set(i+slip, j, new_col);
+//       }
+//       else{
+//         let new_col = color(150,50,68);
+//         //wave
+//         let wave = sin(j*2);
+//         let slip = map(wave, -6,6, -OFFSET+10 , OFFSET+10 );
+//         //pix = sourceImg.get(i+slip, j);
+//         set(i+slip, j, new_col); 
 //       }
 
-      
 //       set(i, j, pix);
+
 //     }
 //   }
 
 
 //   renderCounter = renderCounter + num_lines_to_draw;
 //   updatePixels();
-
 //   // print(renderCounter);
 //   if(renderCounter > Y_STOP) {
 //     console.log("Done!")
@@ -170,9 +178,14 @@ function keyTyped() {
 //   }
 // }
 
-
 // function keyTyped() {
 //   if (key == '!') {
 //     saveBlocksImages();
 //   }
 // }
+
+
+
+
+
+
